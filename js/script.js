3657a12b7245f568ac52b80d3309a49b1a2b449a
maxrnc <max.mrnc@gmail.com>
2025-06-02 19:50:21 -0300

    Atividade 02
    Atividade destinada a treinar algumas funcionalidades do JS

---
+++ js/script.js
@@ -0,0 +1,7 @@
+function multiplicar() {
+    var num1 = Number(document.getElementById("num1").value);
+    var num2 = Number(document.getElementById("num2").value);
+
+    document.getElementById("resultado").innerHTML = num1 * num2;
+}
+
